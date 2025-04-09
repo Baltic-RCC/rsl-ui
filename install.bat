8e6fab6b778b881b1994b64fcd5a077f01fa0b0d
kristjan.vilgo <kristjan.vilgo@gmail.com>
2025-04-09 15:38:16 +0300

    Simple direct on pyhton install script added

--- install.bat
+++
@@ -1,5 +0,0 @@
-docker build -t validator-ui .
-docker run -d -p 8050:8050 --name validator validator-ui
-docker ps
-
-
---
+++ install_on_python.bat
@@ -0,0 +1,3 @@
+:: Used for simple testing and running directly on python
+py -m pip install -r requirements.txt
+py web-app.py
